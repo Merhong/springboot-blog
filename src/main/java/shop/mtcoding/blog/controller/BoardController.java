06501332c8045fdac6cd282f2f72923dd0ed8e27
Merhong <zx2938@gmail.com>
2023-07-28 09:15:18 +0900

    코드컨벤션

--- src/main/java/shop/mtcoding/blog/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -3,21 +3,32 @@ package shop.mtcoding.blog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Controller 어노테이션
+ * 1. 컴포넌트 스캔
+ * 2. 리턴되는 값 : view 파일
+ */
 @Controller
 public class BoardController {
 
-    @GetMapping({ "/", "/board" })
+    // Browser URL : IP주소:포트번호/ or /board 입력시 호출
+    @GetMapping({"/", "/board"})
     public String index() {
+        // view 파일 호출, index
         return "index";
     }
 
-    @GetMapping({ "/board/saveForm" })
+    // Browser URL : IP주소:포트번호/board/saveForm 입력시 호출
+    @GetMapping({"/board/saveForm"})
     public String saveForm() {
+        // view 파일 호출, board/saveForm
         return "board/saveForm";
     }
 
-    @GetMapping({ "/board/1" })
+    // Browser URL : IP주소:포트번호/board/board/1 입력시 호출
+    @GetMapping({"/board/1"})
     public String detail() {
+        // view 파일 호출, board/detail
         return "board/detail";
     }
 
--- src/main/java/shop/mtcoding/blog/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog/controller/UserController.java
@@ -4,28 +4,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+/**
+ * Controller 어노테이션
+ * 1. 컴포넌트 스캔
+ * 2. return 되는 값 : view 파일
+ */
+
 @Controller
 public class UserController {
 
-    @GetMapping("/join")
-    public String joinForm() {
+    // x-www-form-urlencoded 형식으로 들어오는 데이터 받아서 콘솔로 출력하기.
+    @PostMapping("/join")
+    public void join(String username, String password, String email) {
+        System.out.println("이름 : " + username); // username
+        System.out.println("패스워드 : " + password); // password
+        System.out.println("E-mail : " + email); // email
+    }
 
+    // 브라우저 입력 : GET 요청!
+    // Browser URL : IP주소:포트번호/join 입력시 호출
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        // view 파일 호출 user/joinForm 파일 호출
         return "user/joinForm";
     }
 
+    // Browser URL : IP주소:포트번호/login 입력시 호출
     @GetMapping("/login")
     public String loginForm() {
-
+        // view 파일 호출 user/loginForm 파일 호출
         return "user/loginForm";
     }
 
+    // Browser URL : IP주소:포트번호/user/updateForm 입력시 호출
     @GetMapping("/user/updateForm")
     public String updateForm() {
+        // view 파일 호출 user/updateForm 파일 호출
         return "user/updateForm";
     }
 
+    // Browser URL : IP주소:포트번호/logout 입력시 호출
     @GetMapping("/logout")
     public String logout() {
+        // view 파일 호출 -> Welcome File List에서 "/"가 index로 지정되어 있음!
+        // redirect는 스프링에서 지원하는 문법
         return "redirect:/";
     }
 
